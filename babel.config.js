b50f95fbb28091f498ba8a563952e7f1304a567b
natively.dev <api@natively.dev>
2026-04-19 13:49:55 +0000

    fix: Fix app display issue

--- babel.config.js
+++ babel.config.js
@@ -1,36 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
-
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          root: ['./'],
-          extensions: [
-            '.ios.ts',
-            '.android.ts',
-            '.ts',
-            '.ios.tsx',
-            '.android.tsx',
-            '.tsx',
-            '.jsx',
-            '.js',
-            '.json',
-          ],
-          alias: {
-            '@': './',
-            '@components': './components',
-            '@style': './style',
-            '@hooks': './hooks',
-            '@types': './types',
-            '@contexts': './contexts',
-            '@lib': './lib',
-          },
-        },
-      ],
-      '@babel/plugin-proposal-export-namespace-from',
-    ],
   };
 };
